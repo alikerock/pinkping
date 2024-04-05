0b7cc0189e54a769f34cb23a6541ffdeaff3363b
rock's ezweb <alikerock@gmail.com>
2024-04-05 13:18:17 +0900

    category 출력 완성

--- admin/product/category.php
+++ admin/product/category.php
@@ -31,19 +31,13 @@ print_r($cate1);
       <div class="col-md-4">
 
         <select class="form-select" aria-label="중분류" id="cate2">
-          <option selected disabled>중분류</option>
-          <!-- <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option> -->
+
         </select>
       </div>
       <div class="col-md-4">
 
         <select class="form-select" aria-label="소분류" id="cate3">
-          <option selected>소분류</option>
-          <!-- <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option> -->
+
         </select>
       </div>
 
@@ -52,8 +46,68 @@ print_r($cate1);
 </div>
 <script>
   $('#cate1').change(function() {
+    makeOption($(this), 2, '중분류', $('#cate2'));
+  });
+  $('#cate2').change(function() {
+    makeOption($(this), 3, '소분류', $('#cate3'));
+  });
+  $('#cate3').change(function() {
 
   });
+
+  /*
+  async function makeOption(e, step, category, target) {
+    let cate = e.val();
+    let data = {
+      cate: cate,
+      step: step,
+      category: category
+    };
+    console.log(data);
+
+    try {
+      const response = await fetch('printOption.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(res => {
+          return res;
+        })
+        .then(result => {
+          console.log(result);
+        });
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  */
+
+  function makeOption(e, step, category, target) {
+    let cate = e.val();
+    //console.log(cate);
+    // 비동기 방식으로 printOption 값 3개(cate, step, category) 일시키고, 결과가 나오면 target에 html 태그를 생성
+    let data = {
+      cate: cate,
+      step: step,
+      category: category
+    }
+    console.log(data);
+    $.ajax({
+      async: false, // sucess의 결과가 나오면 작업 수행
+      type: 'post',
+      data: data,
+      url: 'printOption.php',
+      dataType: 'html',
+      success: function(result) {
+        console.log(result);
+        target.html(result);
+      }
+    })
+  }
 </script>
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/footer.php';
