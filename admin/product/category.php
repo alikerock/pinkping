1635ce7017549748af0cd4240c57d2f612f8e170
rock's ezweb <alikerock@gmail.com>
2024-04-05 15:20:38 +0900

    대분류 등록 UI 추가

--- admin/product/category.php
+++ admin/product/category.php
@@ -43,7 +43,97 @@ while ($row = $result->fetch_object()) {
 
     </div>
   </form>
-</div>
+  <div class="buttons mt-3">
+    <!-- 대분류 등록 버튼 -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate1Modal">
+      대분류 등록
+    </button>
+
+    <!--대분류 등록 Modal -->
+    <div class="modal fade" id="cate1Modal" tabindex="-1" aria-labelledby="cate1ModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="cate1ModalLabel">대분류 등록</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body row">
+            <div class="col">
+              <input type="text" class="form-control" id="code1" name="code1" placeholder="코드명 입력">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" id="name1" name="name1" placeholder="대분류명 입력">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+            <button type="submit" class="btn btn-primary" data-step="1">등록</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 중분류 등록 버튼 -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate2Modal">
+      중분류 등록
+    </button>
+
+    <!-- 중분류 등록 Modal -->
+    <div class="modal fade" id="cate2Modal" tabindex="-1" aria-labelledby="cate2ModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="cate2ModalLabel">중분류 등록</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body row">
+            <div class="col">
+              <input type="text" class="form-control" id="code2" name="code2" placeholder="코드명 입력">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" id="name2" name="name2" placeholder="중분류명 입력">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+            <button type="submit" class="btn btn-primary" data-step="2">등록</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 소분류 등록 버튼 -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate3Modal">
+      소분류 등록
+    </button>
+
+    <!--소분류 등록 Modal -->
+    <div class="modal fade" id="cate3Modal" tabindex="-1" aria-labelledby="cate3ModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="cate3ModalLabel">소분류 등록</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body row">
+            <div class="col">
+              <input type="text" class="form-control" id="code1" name="code1" placeholder="코드명 입력">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" id="name1" name="name1" placeholder="대분류명 입력">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+            <button type="submit" class="btn btn-primary" data-step="3">등록</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div><!-- //container -->
+
 <script>
   $('#cate1').change(function() {
     makeOption($(this), 2, '중분류', $('#cate2'));
@@ -82,6 +172,25 @@ while ($row = $result->fetch_object()) {
     } catch (error) {
       console.error('Error:', error);
     }
+  }
+
+  let categorySubmitBtn = $(".modal button[type='submit']");
+
+  categorySubmitBtn.click(function() {
+    let step = $(this).attr('data-step');
+    save_category(step);
+  });
+
+  function save_category(step) {
+    let pcode = $(`#code${step}`).val();
+    let name = $(`#name${step}`).val();
+
+    let data = {
+      name: name,
+      pcode: pcode,
+      step: step
+    }
+    console.log(data);
   }
 
   /*
