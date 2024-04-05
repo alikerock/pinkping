a44fbf6ba15efcb6fe591cfb66d4c1df874cff17
rock's ezweb <alikerock@gmail.com>
2024-04-05 10:46:17 +0900

    error log add

--- inc/dbcon.php
+++ inc/dbcon.php
@@ -1,13 +1,15 @@
 <?php
 $hostname = "localhost";
 $dbuserid = "pinkping";
-$dbpasswd = "12345";
+$dbpasswd = "123451";
 $dbname = "pinkping";
-
 
 $mysqli = new mysqli($hostname, $dbuserid, $dbpasswd, $dbname);
 
-
 if ($mysqli->connect_errno) {
-  die('Connect Error: ' . $mysqli->connect_error);
+  // 오류 메시지를 서버의 로그 파일에 기록
+  error_log('Database connection failed: ' . $mysqli->connect_error);
+
+  // 사용자에게는 일반적인 오류 메시지만 표시
+  die('Sorry, we are experiencing some technical difficulties. Please try again later.');
 }
