0a2437a05bfd37e14b6688873646ccebae6fa8be
rock's ezweb <alikerock@gmail.com>
2024-04-11 11:21:36 +0900

    파일 업로드 - 파일명 난수

--- admin/product/product_save_image.php
+++ admin/product/product_save_image.php
@@ -22,3 +22,7 @@ if (strpos($_FILES['savefile']['type'], 'image') === false) {
   exit;
 }
 //파일 업로드
+$save_dir = $_SERVER['DOCUMENT_ROOT'] . '/pinkping/admin/upload';
+$fiename = $_FILES["savefile"]["name"]; //insta.jpg
+$ext = pathinfo($fiename, PATHINFO_EXTENSION); //jpg
+$newfilename = date("YmdHis")
