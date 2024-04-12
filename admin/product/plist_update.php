a689b9c2f60465526202d4cfa6e7df8c01b4477b
rock's ezweb <alikerock@gmail.com>
2024-04-12 14:42:04 +0900

    일괄 수정 sql

--- admin/product/plist_update.php
+++ admin/product/plist_update.php
@@ -10,7 +10,19 @@ $isbest = $_REQUEST['isbest'] ?? [];
 $isrecom = $_REQUEST['isrecom'] ?? [];
 $status = $_REQUEST['status'] ?? [];
 
-print_r($pid);
+foreach($pid as $p){
+  $ismain = $ismain[$p] ?? 0;
+  $isnew = $isnew[$p] ?? 0;
+  $isbest = $isbest[$p] ?? 0;
+  $isrecom = $isrecom[$p] ?? 0;
+  $status = $status[$p] ?? 0;
 
-print_r($ismain);
+  $sql  = "UPDATE products set ismain = {$ismain}, isnew = {$isnew}, isbest = {$isbest},isrecom = {$isrecom},status = {$status} WHERE pid ={$p}";
+  $result = $mysqli -> query($sql);
+}
+if($result){
+  echo "<script>alert('일괄 수정 완료');history.back();</script>";  
+} else{
+  echo "<script>alert('일괄 수정 실패');history.back();</script>";
+}
 
