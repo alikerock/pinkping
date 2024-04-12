167cafc706fbedcb9b68df8e6b9e69c302a6b787
rock's ezweb <alikerock@gmail.com>
2024-04-12 15:48:58 +0900

    pagenation 목록 추출

--- admin/product/product_list.php
+++ admin/product/product_list.php
@@ -41,17 +41,37 @@ if($search_keyword){
 }
 
 
-$sql = "SELECT * FROM products where 1=1"; //모든 상품 조회 쿼리
-$sql .= $search_where;
-$order = " order by pid desc";
-$sql .= $order;
-$result = $mysqli->query($sql);
-
 $cntsql = "SELECT COUNT(*) AS cnt FROM products where 1=1"; 
 $cntsql .= $search_where;
 $cntresult = $mysqli->query($cntsql);
 $cntrow = $cntresult->fetch_object();
-$count = $cntrow -> cnt;
+$count = $cntrow -> cnt; //검색 개수 출력
+
+//페이지네이션 시작
+$pageNumber = $_GET['pageNumber'] ?? 1;
+$pageCount = $_GET['pageCount'] ?? 10;
+$startLimit = ($pageNumber -1)*$pageCount;
+$endLimit = $pageCount ;
+$firstPageNumber = $_GET['firstPageNumber'] ?? 0;
+
+$block_ct = 5;  //12345, 678910
+$block_num = ceil($pageNumber/$block_ct);  //65개수 1/5 0.2 1
+$block_start = (($block_num - 1) * $block_ct) + 1; 
+$block_end = $block_start + $block_ct - 1;
+
+$total_page = ceil($count / $pageCount); 
+if($block_end > $total_page) $block_end = $total_page;
+
+$total_block = ceil($total_page/$block_ct); 
+
+$sql = "SELECT * FROM products where 1=1"; //모든 상품 조회 쿼리
+$sql .= $search_where;
+$order = " order by pid desc";
+$sql .= $order;
+$limit = " LIMIT $startLimit, $endLimit";
+$sql .= $limit;
+
+$result = $mysqli->query($sql);
 
 while ($rs = $result->fetch_object()) {
   $rsArr[] = $rs;
