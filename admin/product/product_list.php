7abf344437f99c39287821ae59d11ccd29dfb397
rock's ezweb <alikerock@gmail.com>
2024-04-12 09:42:05 +0900

    상품 목록 카테고리 선택 추가

--- admin/product/product_list.php
+++ admin/product/product_list.php
@@ -1,11 +1,85 @@
 <?php
+session_start();
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/admin/inc/admin_check.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
 
+$sql = "SELECT * FROM category where step = 1";
+$result = $mysqli->query($sql);
+while ($row = $result->fetch_object()) {
+  $cate1[] = $row;
+}
 ?>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+
 <div class="container">
   <h1>상품 목록</h1>
+  <div class="category row">
+    <div class="col">
+
+      <select class="form-select" aria-label="대분류" id="cate1" name="cate1" required>
+        <option selected disabled>대분류</option>
+        <?php
+        foreach ($cate1 as $c1) {
+        ?>
+
+          <option value="<?= $c1->code; ?>"><?= $c1->name; ?></option>
+
+        <?php
+        }
+        ?>
+
+      </select>
+    </div>
+    <div class="col">
+
+      <select class="form-select" aria-label="중분류" id="cate2" name="cate2">
+
+      </select>
+    </div>
+    <div class="col">
+
+      <select class="form-select" aria-label="소분류" id="cate3" name="cate3">
+
+      </select>
+    </div>
+  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
   <a href="product_up.php" class="btn btn-primary">상품 등록</a>
 </div>
+
+<script src="/pinkping/admin/js/makeoption.js"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/footer.php';
 ?>
