f0387808af0abb87b00a3160a67a2c6f8c4f4067
alikerock <alikerock@gmail.com>
2024-04-15 15:43:05 +0900

    상세페이지 추가이미지 출력

--- admin/product/product_view.php
+++ admin/product/product_view.php
@@ -4,30 +4,40 @@ session_start();
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/admin/inc/admin_check.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
 
+$pid = $_GET['pid']; 
+$sql = "SELECT * FROM products WHERE pid = {$pid}";
+$result = $mysqli -> query($sql);
+$rs = $result->fetch_object();
+
+$imgSql = "SELECT * FROM product_image_table WHERE pid = {$pid}";
+$imgrs = $mysqli -> query($imgSql);
+
+while ($irs = $imgrs->fetch_object()) {
+    $rsArr[] = $irs;
+}
+
+
+
 ?>
 <style>
-    .thumbnail{
-        text-align: center;        
-    }
-    .thumbnail img{
-        max-width: 400px;
-    }
-    .addedImage img{
-        max-width: 200px;
-    }
+    .thumbnail{text-align: center;}
+    .thumbnail img{max-width: 400px;}
+    .addedImage img{max-width: 200px;}
 </style>
 
 <div class="container">
-    <h2></h2> <!-- 제품명 -->
+    <h2><?= $rs->name; ?></h2> <!-- 제품명 -->
     <div class="thumbnail">
-        <img src="" alt=""><!-- 대표이미지 -->
+        <img src="<?= $rs->thumbnail; ?>" alt=""><!-- 대표이미지 -->
     </div>
     <div class="addedImage"> <!-- 추가이미지 -->
-        <!--
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        -->
+        <?php
+            foreach($rsArr as $ra){
+                ?>
+                <img src="/pinkping/admin/upload/<?= $ra->filename; ?>" alt="">
+                <?php
+            }
+        ?>
     </div>
     <div class="description"><!-- 상세설명 -->
 
