80de63c882d7d376d239043ba28f8035981a5cbd
alikerock <alikerock@gmail.com>
2024-04-17 13:15:42 +0900

    admin index 생성

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/admin/inc/admin_check.php';
+?>
+
+<div class="container">
+    <h1>대쉬보드</h1>
+    <div>
+        <?php echo '반갑습니다.'.$_SESSION['AUNAME'].'님' ; ?>
+        <a href="logout.php">logout</a>
+    </div>
+</div>
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/footer.php';
+?>
