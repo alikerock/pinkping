c5ee31a79ec1081a80f2cc9e11ba4d133a9c1c2a
alikerock <alikerock@gmail.com>
2024-04-17 10:42:58 +0900

    회원가입 폼 생성

--- member/signup.php
+++ member/signup.php
@@ -2,21 +2,26 @@
 session_start();
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
 ?>
-
-userid
-email
-passwd
-
 <div class="container">
-    <form action="">
+    <h2>회원가입</h2>
+    <form action="signup_ok.php" method="POST">
         <div class="form-floating mb-3">
-            <input type="email" class="form-control" name="username" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <input type="text" class="form-control" name="username" id="username" placeholder="username">
+            <label for="username">username</label>
         </div>
-        <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" name="userid" id="userid" placeholder="userid">
+            <label for="userid">userid</label>
         </div>
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control" name="email" id="email" placeholder="email">
+            <label for="email">email</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" class="form-control" name="passwd" id="passwd" placeholder="passwd">
+            <label for="passwd">passwd</label>
+        </div>
+        <button class="btn btn-primary">회원가입</button>
     </form>
 </div>
 
