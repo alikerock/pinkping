4ca315828e6de7c55884e4ca670746cb254eba84
alikerock <alikerock@gmail.com>
2024-04-17 13:19:41 +0900

    chart js 추가

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,12 @@ session_start();
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/admin/inc/admin_check.php';
 ?>
+<style>
+    #myChart{
+        width: 500px;
+        height: 400px;
+    }
+</style>
 
 <div class="container">
     <h1>대쉬보드</h1>
@@ -10,6 +16,34 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/admin/inc/admin_check.php';
         <?php echo '반갑습니다.'.$_SESSION['AUNAME'].'님' ; ?>
         <a href="logout.php">logout</a>
     </div>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+    });
+    </script>
 </div>
 
 <?php
