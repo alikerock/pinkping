a135902172642207b44159b15239fe34ac20f68b
alikerock <alikerock@gmail.com>
2024-04-17 10:42:48 +0900

    front index 생성

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
+?>
+<div class="container">
+    <h1>PINKPING</h1>
+    
+</div>
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/footer.php';
+?>
