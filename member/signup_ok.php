8dc36707e77f95c8427a22704d2e38d913ebeb98
alikerock <alikerock@gmail.com>
2024-04-17 12:30:55 +0900

    쿠폰방행

--- member/signup_ok.php
+++ member/signup_ok.php
@@ -1,5 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/dbcon.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/coupon_func.php';
 
 $username = $_POST['username'];
 $userid = $_POST['userid'];
@@ -11,26 +12,6 @@ $sql = "INSERT INTO members (username, userid, email, passwd, regdate) VALUES ('
 $result = $mysqli -> query($sql);
 if($result){
     //회원가입 축하 쿠폰 발행
-    $csql = "SELECT * FROM coupons WHERE cid=1";//축하 쿠폰 조회
-    $cresult = $mysqli -> query($csql);
-    $crs =  $cresult->fetch_object();
-
-    $due_date = date("Y-m-d 23:59:59", strtotime("+30 days"));
-
-    $ucSql = "INSERT INTO user_coupons (couponid,userid,status,use_max_date,regdate,reason) VALUES (
-        '{$crs->cid}',
-        '{$userid}',
-        1,
-        '{$due_date}',
-        now(),
-        '회원가입'
-    )";
-    $ucResult = $mysqli -> query($ucSql);
-    $couponName = $crs->coupon_name;
-
-    echo "<script>
-        alert('회원가입 완료!, $couponName 쿠폰이 발행되었습니다.');
-        location.href='/pinkping/index.php';
-    </script>";
+    issue_coupon($mysqli, $userid, 1, '회원가입');
 }
 ?>
