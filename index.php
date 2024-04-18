b046f53989660b7bb87f7eb1d02b79e194a86707
alikerock <alikerock@gmail.com>
2024-04-18 11:13:51 +0900

    메인상품 출력 php 추가

--- index.php
+++ index.php
@@ -210,25 +210,34 @@ order by pid desc limit 0, 6
 
             <div class="container">
                 <div class="row karl-new-arrivals">
-
+                    <?php
+                    if(isset($rsc)){
+                        foreach($rsc as $item){
+                    ?>
                     <!-- Single gallery Item Start -->
                     <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig"
                         data-wow-delay="0.2s">
                         <!-- Product Image -->
                         <div class="product-img">
-                            <img src="img/product-img/product-1.jpg" alt="">
+                            <img src="<?= $item->thumbnail; ?>" alt="">
                             <div class="product-quicview">
                                 <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
                             </div>
                         </div>
                         <!-- Product Description -->
                         <div class="product-description">
-                            <h4 class="product-price">$39.90</h4>
-                            <p>Jeans midi cocktail dress</p>
+                            <h4 class="product-price"><?= $item->price; ?></h4>
+                            <p><?= $item->name; ?></p>
                             <!-- Add to Cart -->
                             <a href="#" class="add-to-cart-btn">ADD TO CART</a>
                         </div>
                     </div>
+                    <?php
+                        }
+                    } else {
+                        echo "<p>조회 상품이 없습니다.</p>";
+                    }
+                    ?>
                     
                 </div>
             </div>
