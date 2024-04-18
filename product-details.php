9f5e846854fe982185ddb127144095e9744e12d9
alikerock <alikerock@gmail.com>
2024-04-18 16:41:08 +0900

    최근본 상품 쿠키생성

--- product-details.php
+++ product-details.php
@@ -10,29 +10,24 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/head.php';
 //var_dump(json_decode("17,15")); // "17,15" -> 17%2C15
 $pid = $_GET['pid'];
 
+$rvcArr = [];
+
 if(isset($_COOKIE['recent_viewed'])){
     $rvcArr = json_decode($_COOKIE['recent_viewed']);//string -> array
     // $rvcArr = explode(",", $rvc);
     if(!in_array($pid, $rvcArr)){//이미 본 상품이 아니라면
         //본상품이 3개 이상이라면, 첫상품을 제거
         if(sizeof($rvcArr) >= 3){
-            unset($rvcArr[0]);
-            $rvcArr = array_values($rvcArr); //인덱스 재정렬
+            array_shift($rvcArr);
+            //unset($rvcArr[0]);
+            //$rvcArr = array_values($rvcArr); //인덱스 재정렬
             //ksort($rvcArr);  abc, 가나다 순으로 재정렬
         }
-        array_push($rvcArr, $pid); //배열에 마지막에 추가
-
-        // implode(',',$rvcArr); //'12,17,5'
-        $rvcStr = json_encode($rvcArr);//[12,17,5] -> '[12,17,5]'
-        setcookie('recent_viewed', $rvcStr, time()+86400, "/");
     }
-} else{
-    $rvcArr = [];
-    array_push($rvcArr, $pid);
-    $rvcStr = json_encode($rvcArr);
-    setcookie('recent_viewed', $rvcStr, time()+86400, "/");  // 쿠키가 24시간 지속됨.
-}
-
+} 
+array_push($rvcArr, $pid); //배열에 마지막에 추가
+$rvcStr = json_encode($rvcArr);//[12,17,5] -> '[12,17,5]'
+setcookie('recent_viewed', $rvcStr, time()+86400, "/");
 
 ?>
 
