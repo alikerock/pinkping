f7e567bdf8efc6db04b7ce7c91c668bdd134d30f
alikerock <alikerock@gmail.com>
2024-04-23 10:56:45 +0900

    cart total 출력

--- inc/head.php
+++ inc/head.php
@@ -20,7 +20,7 @@ $ssid = session_id();
 
 // $cartSql = "SELECT * FROM cart WHERE ssid = '{$ssid}'";
 
-$cartSql = "SELECT p.thumbnail,p.name,p.price,c.cnt,c.options
+$cartSql = "SELECT p.thumbnail,p.name,p.price,c.cnt,c.options,c.total
             FROM products p
                 INNER JOIN cart c
                 ON c.pid = p.pid
