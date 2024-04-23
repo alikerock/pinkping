6658158a9809cbc8b0dec7c076326fa155b9a136
alikerock <alikerock@gmail.com>
2024-04-23 10:24:03 +0900

    장바구니 가격 출력 추가

--- inc/head.php
+++ inc/head.php
@@ -20,7 +20,7 @@ $ssid = session_id();
 
 // $cartSql = "SELECT * FROM cart WHERE ssid = '{$ssid}'";
 
-$cartSql = "SELECT p.thumbnail,p.name,c.cnt,c.options
+$cartSql = "SELECT p.thumbnail,p.name,p.price,c.cnt,c.options
             FROM products p
                 INNER JOIN cart c
                 ON c.pid = p.pid
