20bf06f45289325fea3076456e4c656a1cd3220c
alikerock <alikerock@gmail.com>
2024-04-25 13:12:58 +0900

    순서변경

--- checkout.php
+++ checkout.php
@@ -8,12 +8,19 @@ $ucid = $_POST['coupon']??'';
 
 $sql = "INSERT INTO orders (userid,pid,total_price,regdate) VALUES ('{$userid}','{$pid}',{$grand_total},now())";
 $result = $mysqli -> query($sql);
+
+
+if(isset($ucid) && $ucid !==''){
+    $sql = "UPDATE user_coupons SET status = -1 WHERE ucid = $ucid";
+    $mysqli -> query($sql);
+}
+//카트비우기
+$delsql = "DELETE FROM cart WHERE userid = '{$userid}'";
+$delresult = $mysqli -> query($delsql);
+
 if($result){
     echo "<script>
         alert('주문이 완료되었습니다.');
         location.href = '/pinkping/index.php';
     </script>";
-}
-if(isset($ucid) && $ucid !==''){
-    
 }
